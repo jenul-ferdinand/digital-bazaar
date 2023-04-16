dda7ea7dc00ace87d40e1f04911967949a81bc1d
Jenul Ferdinand <jenul15ferdinand@gmail.com>
2023-04-16 23:37:45 +1000

    Refactored javadocs folder to docs

--- javadocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"main"},{"l":"main.controllers"},{"l":"main.models.devices"},{"l":"main.models.purchase"},{"l":"main.utility"}];updateSearchResults();
